97a20fe194230adbe21f8863a609eb03dd3b0718
Botir Mahmudov <botir6@gmail.com>
2026-03-18 18:37:26 +0500

    Update app.py

--- app.py
+++ app.py
@@ -8,8 +8,9 @@ DATABASE_URL = os.environ.get("DATABASE_URL")
 
 
 def get_connection():
-    return psycopg2.connect(DATABASE_URL)
-
+    if not DATABASE_URL:
+        raise Exception("DATABASE_URL topilmadi")
+    return psycopg2.connect(DATABASE_URL, sslmode="require")
 
 def init_db():
     conn = get_connection()
