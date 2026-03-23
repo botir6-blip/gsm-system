be7d07a4ec111f8db7cd648b5e85e53d70095ef9
Botir Mahmudov <botir6@gmail.com>
2026-03-23 06:45:06 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,12 @@
 from flask import Flask
 from db import init_db
-
-# routes
 from routes.transactions import transactions_bp
 
 app = Flask(__name__)
 app.secret_key = "supersecretkey"
 
-# DB
 init_db()
 
-# BLUEPRINTS
 app.register_blueprint(transactions_bp)
 
 if __name__ == "__main__":
