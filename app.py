be0a989e5d1c17bf7ae1851c956dfd7bef91b91d
Botir Mahmudov <botir6@gmail.com>
2026-03-23 06:46:22 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
+import os
 from flask import Flask
 from db import init_db
 from routes.transactions import transactions_bp
 
 app = Flask(__name__)
-app.secret_key = "supersecretkey"
+app.secret_key = os.environ.get("SECRET_KEY", "supersecretkey")
 
 init_db()
-
 app.register_blueprint(transactions_bp)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
